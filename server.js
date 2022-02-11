0da5ed7d4ab3fef18b1501e9a31f3a766fc53bf2
Aiden Bai <aiden.bai05@gmail.com>
2022-02-11 00:23:17 -0800

    cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-  cors: {},
+  cors: { origin: '*' },
 });
 
 io.on('connection', (socket) => {
